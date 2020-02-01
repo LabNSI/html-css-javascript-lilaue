7053b04ff1e87cfbcc3b19bbe9432471593f93ec
lilaue <53794630+lilaue@users.noreply.github.com>
2020-02-01 20:48:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function init () {
   // La fonction résultat() est exécutée lors de la soumission du formulaire
   // Indiquer le nom du formilaire et l'évenement correspondant à sa soumission
-  document.getElementById("nom_du_formulaire")._evenement_soumission = resultat;
+  document.getElementById("terminale").onsumbit = resultat;
 
   //Récupération des variables et de leurs valeurs dans l'URL :
   // ex : //formulaire.html?prenom=Albert&nom=Dupond&classe=1G2&LITT_ANGLAIS=LITT.+ANGLAIS&NUMERIQUE_SC_INFORM=NUMERIQUE+SC.INFORM
@@ -13,40 +13,40 @@ function init () {
 
   // Compléter le test
   // si sReq n'est pas une chaine vide
-  if(sReq ______)
+  if(sReq != "")
   {
       const good = "NUMERIQUE_SC_INFORM";
       // La fonction split découpe une chaine de caractères (string) et retourne un tableau (array)
       // Quel caractère sépare les ensemble variable=valeur ?
       // Utiliser ce caractère pour découper la chaine sReq
-      var aReq = sReq.split("__");
+      var aReq = sReq.split("&");
       var mess = "";
       // Boucle sur les variables
       var aVar = [];
       for (var i=0;i<aReq.length;i++) {
         // Quel caractère sépare une variable de sa valeur ?
         // Utiliser ce caractère pour découper la chaine aReq[i]
-        aVar[i] = aReq[i].split("__");
+        aVar[i] = aReq[i].split("=");
         // Observer dans la console
         console.log("aVar["+i+"][0] = "+aVar[i][0]+"   "+"aVar["+i+"][1] = "+aVar[i][1]);
       }
       //Construire la chaine suivante en utilisant les valeurs récupérées
       // mess = valeur_du prenom + " " + valeur_du_nom + " " + valeur_de_classe + " : "
-      mess = _______ + " " + ______ + " de " + ______ + " : ";
+      mess = aVar[0][1] + " " + aVar[1][1] + " de " + aVar[2][1] + " : ";
 
       //Si au moins une des spécialités est = "NUMERIQUE_SC_INFORM"
       // Ajouter à mess "Bon choix !"
       // Sinon ajouter à mess "Mauvais choix !"
       if(aVar[3][0] == good || aVar[4][0] == good)
       {
-        mess += ______________;
+        mess += "Bon choix !";
       }
       else {
-        mess += ______________;
+        mess += "Mauvais choix !";
       }
 
       // Ajouter le code HTML mess à l'élément d'ID='resultat'
-      document.getElementById('______')._________ = _______;
+      document.getElementById('resultat').innerHTML= mess;
   }
 }
 
@@ -64,43 +64,43 @@ function resultat() {
     message += "\n- Prénom";
   }
 
-  // ?
+  // Vérifier si le nom a été saisie. Si non (chaine vide), ajouter "\n - Nom" à message
   if(f.elements["nom"].value == "")  {
     message += "\n- Nom";
   }
 
-  // ?
+  // Vérifier si la classe a été saisie. Si non (chaine vide), ajouter "\n - Classe" à message
   if(!f.elements[2].checked && !f.elements[3].checked && !f.elements[4].checked){
     message += "\n- Classe";
   }
 
   var cpt = 0;
   // Les éléments du formulaire 5 à 11 sont les cases à cocher du choix de spécialités
-  // ?
+  // vérifie si les spé sont sélectionnées
   for (var i=5; i < 12; i++)  {
     if(f.elements[i].checked){
       cpt++;
     }
   }
 
-  // ?
+  // vérifie si 2 spé sont sélectionnées
   if(cpt!=2){
     message += "\n- Deux spécialités";
   }
 
-  // ?
+  // envoie une alerte si les spé ne sont pas choisies
   if(message != "Compléter les champs :"){
-    alert(message);
+    alert("message");
     // Mettre fin à lexécution de la fonction
-    return 0;
+    return "";
   }
 
   // Préparer le message à afficher
   message = f.elements["prenom"].value + " " +
-            f.elements["nom"].value + " de " +
+            f.elements["nom"].value + " de" +
             f.elements["classe"].value;
 
-  // ?
+  //  Vérifier si NUMERIQUE_SC_INFORM a été chosi si c'est le cas Tu fais le bon choix s'affiche sinon le message Es-tu sur de ton choix ? s'affiche
   if(f.elements["NUMERIQUE_SC_INFORM"].checked){
     message += "\nTu fais le bon choix !";
   }
